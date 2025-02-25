0f829301a947e0ae86bff2e140773e3277a39fbf
szabo-armin <szaboar33@gmail.com>
2025-02-25 18:12:09 +0100

    Test draw 2

--- sza_h3q_turtlesim/draw.py
+++ sza_h3q_turtlesim/draw.py
@@ -3,42 +3,46 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist
 import time
 
-class TriangleDrawer(Node):
+class SquareDrawer(Node):
 
     def __init__(self):
-        super().__init__('triangle_drawer')
+        super().__init__('square_drawer')
         self.cmd_pub = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
-        self.draw_triangle()
+        self.draw_square()
 
-    def draw_triangle(self):
+    def draw_square(self):
         cmd_msg = Twist()
-        side_length = 2.0
-        turn_time = 1.0
-        linear_speed = 2.0
-        angular_speed = 1.57
+        side_length = 2.0  # Idő egy oldal rajzolására (másodperc)
+        turn_time = 1.0    # Idő a forduláshoz
+        linear_speed = 2.0 # Sebesség előrehaladáskor
+        angular_speed = 1.57  # kb. 90 fok/sec (pi/2 radián)
 
-        for _ in range(3):
+        for _ in range(4):  # 4 oldal -> 4 mozgás + 4 fordulás
+            # Egyenes vonal rajzolása
             cmd_msg.linear.x = linear_speed
             cmd_msg.angular.z = 0.0
             self.cmd_pub.publish(cmd_msg)
             time.sleep(side_length)
 
+            # Fordulás (90 fok)
             cmd_msg.linear.x = 0.0
             cmd_msg.angular.z = angular_speed
             self.cmd_pub.publish(cmd_msg)
             time.sleep(turn_time)
 
+        # Állítsuk meg a teknőst a végén
         cmd_msg.linear.x = 0.0
         cmd_msg.angular.z = 0.0
         self.cmd_pub.publish(cmd_msg)
-        self.get_logger().info("Háromszög rajzolása kész!")
+        self.get_logger().info("Kocka rajzolása kész!")
 
 def main(args=None):
     rclpy.init(args=args)
-    triangle_drawer = TriangleDrawer()
-    rclpy.spin(triangle_drawer)  # Az eseményeket figyeli
-    triangle_drawer.destroy_node()
+    square_drawer = SquareDrawer()
+    rclpy.spin_once(square_drawer, timeout_sec=1)  # Csak egyszer fusson le
+    square_drawer.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
+
